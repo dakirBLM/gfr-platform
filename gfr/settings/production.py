d51d199cf80484803f493c5712dbbdab06eac58f
Dhakir <138573154+dakirBLM@users.noreply.github.com>
2026-08-13 13:01:47 +0200

    Update production.py

--- gfr/settings/production.py
+++ gfr/settings/production.py
@@ -24,7 +24,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     host.strip()
-    for host in os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+    for host in os.environ.get('DJANGO_ALLOWED_HOSTS', 'https://gfr-platform.onrender.com').split(',')
     if host.strip()
 ]
 if not ALLOWED_HOSTS:
